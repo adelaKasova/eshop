b342cd0e921d31ff6e975708c9b5a668a867b8b1
Adéla Kašová <adela.kasova@teta.eu>
2026-02-03 17:40:19 +0100

    oprava pomínky v Breadcrumbs

--- src/components/Breadcrums/Breadcrumbs.tsx
+++ src/components/Breadcrums/Breadcrumbs.tsx
@@ -8,7 +8,7 @@ export const Breadcrumbs = ({ breadcrumbs }: BreadcrumbsProps) => {
 
   return (
     <div className="pb-8 flex flex-wrap gap-2 text-sm">
-      {Breadcrumbs === undefined ? <div className="h-3 bg-gray-200 rounded w-28" /> : breadcrumbs?.map((breadcrumb, index) => (
+      {breadcrumbs === undefined ? <div className="h-3 bg-gray-200 rounded w-28" /> : breadcrumbs?.map((breadcrumb, index) => (
         <div key={index} className="gap-2">
           <span>{breadcrumb.category.name}</span>
           <span>{index < (breadcrumbs.length - 1) ? ' > ' : ''}</span>
