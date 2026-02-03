b1b642bf4ce9ca78562e23f5dc3b8015c299ccec
Adéla Kašová <adela.kasova@teta.eu>
2026-02-03 17:38:08 +0100

    oprava class

--- src/components/Breadcrums/Breadcrumbs.tsx
+++ src/components/Breadcrums/Breadcrumbs.tsx
@@ -7,9 +7,7 @@ interface BreadcrumbsProps {
 export const Breadcrumbs = ({ breadcrumbs }: BreadcrumbsProps) => {
 
   return (
-    <div className="cccccbrfjectegugiutjtfjcuhnillffecgevgrlcldc
-    cccccbrfjectinflkrklejibntgjftttjgrelefbehun
-    pb-8 flex flex-wrap gap-2 text-sm">
+    <div className="pb-8 flex flex-wrap gap-2 text-sm">
       {Breadcrumbs === undefined ? <div className="h-3 bg-gray-200 rounded w-28" /> : breadcrumbs?.map((breadcrumb, index) => (
         <div key={index} className="gap-2">
           <span>{breadcrumb.category.name}</span>
